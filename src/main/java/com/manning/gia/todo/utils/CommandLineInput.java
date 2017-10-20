669434e46674217cf19509414cf3ceb4790bb9a5
taichangwei <taicw@cheche365.com>
2017-10-20 15:32:03 +0800

    [Summary]:首次提交todo-app项目

---
+++ src/main/java/com/manning/gia/todo/utils/CommandLineInput.java
@@ -0,0 +1,31 @@
+package com.manning.gia.todo.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum CommandLineInput {
+    FIND_ALL('a'), FIND_BY_ID('f'), INSERT('i'), UPDATE('u'), DELETE('d'), EXIT('e');
+
+    private final static Map<Character, CommandLineInput> INPUTS = new HashMap<>();
+
+    static {
+
+        for (CommandLineInput input : values()) {
+            INPUTS.put(input.getShortCmd(), input);
+        }
+    }
+
+    private final char shortCmd;
+
+    private CommandLineInput(char shortCmd) {
+        this.shortCmd = shortCmd;
+    }
+
+    public char getShortCmd() {
+        return shortCmd;
+    }
+
+    public static CommandLineInput getCommandLineInputForInput(char input) {
+        return INPUTS.get(input);
+    }
+}
